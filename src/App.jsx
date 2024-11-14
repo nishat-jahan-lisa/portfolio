3330120971c475553964fd6308594b267577d873
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2024-11-14 17:37:57 +0100

    Initial commit: Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-white">
+        Hello, Tailwind CSS with Vite!
+      </h1>
+    </div>
+  )
+}
+
+export default App
